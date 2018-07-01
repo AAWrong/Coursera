b33e77c8e0b323aabaad4df2c24aaec6e72f2d70
unknown <tianshi.zhang@mail.utoronto.ca>
2018-06-30 20:36:47 -0400

    first draft

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+<link rel = "stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+<head>
+</head>
+	<body>
+		<ul class="nav nav-tabs justify-content-end">
+		  <li class="nav-item">
+		    <a class="nav-link active" href="#">Home</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Design Philosophy</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link disabled" href="#">Engineering Projects
+		    </a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link disabled" href="#">Techanical Skills
+		    </a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link disabled" href="#">About This Site
+		    </a>
+		  </li>
+
+		</ul>
+		<div class="jumbotron jumbotron-fluid">
+
+  			<div class="container">
+    			<h1 class="display-4 hand-written">I'm a student engineer
+    			</h1>
+    			<p class="lead autograph">-- Aaron Zhang</p>
+  			</div>
+		</div>
+
+
+		<div class="self-intro">
+			<h1> From Beijing to Hamilton, and now, Toronto</h1>
+			<p class="text-self-intro"> 
+			I am from Beijing, China. <br/> 
+			In 2014, I came to Hamilton, Ontario. I spent 2 years there finishing my final 2 years of high school study. <br/>
+			In 2016, I began my study of engineering science program at University of Toronto. <br/>
+			</p>
+			<div class="Beijing">
+				 <image src="Beijing.jpg" height="300" width="400"></image>
+			</div>
+			<div class="Hamilton">
+				 <image src="Hamilton.jpg" height="300" width="400"></image>
+			</div>
+			<div class="Toronto">
+				 <image src="Toronto.jpg" height="300" width="400"></image>
+			</div>
+		</div>
+
+	</body>
+</html>
